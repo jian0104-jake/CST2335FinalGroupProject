b4d38fd3b265e826928ea7682eb71a8a04a43cdb
Xingming Li <li000636@algonquinlive.com>
2020-07-12 17:02:43 -0400

    Add four buttons in MainActivity. Add activity DeezerSongSearchActivity

---
+++ app/src/main/java/com/example/cst2335finalgroupproject/DeezerSongSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.cst2335finalgroupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeezerSongSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deezer_song_search);
+    }
+}
--- app/src/main/java/com/example/cst2335finalgroupproject/MainActivity.java
+++ app/src/main/java/com/example/cst2335finalgroupproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cst2335finalgroupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnGeoDataSource = findViewById(R.id.btnGeoDataSource);
+        btnGeoDataSource.setOnClickListener((view -> {
+
+        }));
+
+        Button btnSoccer = findViewById(R.id.btnSoccer);
+        btnSoccer.setOnClickListener((view -> {
+
+        }));
+
+        Button btnSongLyrics = findViewById(R.id.btnSongLyrics);
+        btnSongLyrics.setOnClickListener((view -> {
+
+        }));
+
+        Button btnDeezer = findViewById(R.id.btnDeezer);
+        btnDeezer.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, DeezerSongSearchActivity.class);
+            startActivity(intent);
+        });
     }
 }
