4ab2438a0fed3361a63e69aa5ee14bf8f39bf047
XingmingLi <li000636@algonquinlive.com>
2020-07-29 15:28:16 -0400

    try to add fav song activity

---
+++ app/src/main/java/com/example/cst2335finalgroupproject/DeezerSongSearch/DeezerFavSongActivity.java
@@ -0,0 +1,15 @@
+package com.example.cst2335finalgroupproject.DeezerSongSearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.cst2335finalgroupproject.R;
+
+public class DeezerFavSongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deezer_fav_song);
+    }
+}
