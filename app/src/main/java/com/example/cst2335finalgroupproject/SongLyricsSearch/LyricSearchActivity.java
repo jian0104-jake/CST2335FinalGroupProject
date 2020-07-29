ac8dfb8d0e7f0101032bebece69a24b02984bdf3
EricWu <ericwoodenman@gmail.com>
2020-07-29 11:42:11 -0400

    SongLyricsSearch Branch. 1. Fix the problem that crush when visit favorite song list in tablet model. 2. Change navigation item to AlertDialog.

--- app/src/main/java/com/example/cst2335finalgroupproject/SongLyricsSearch/LyricSearchActivity.java
+++ app/src/main/java/com/example/cst2335finalgroupproject/SongLyricsSearch/LyricSearchActivity.java
@@ -95,7 +95,7 @@ public class LyricSearchActivity extends AppCompatActivity implements Navigation
         setSupportActionBar(toolBar);
 
         // navigation bar
-        DrawerLayout drawerLayout = findViewById(R.id.lyric_drawer_layout);
+        DrawerLayout drawerLayout = findViewById(R.id.lyric_search_drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
                 drawerLayout, toolBar, R.string.lyric_navigation_open, R.string.lyric_navigation_close);
         drawerLayout.addDrawerListener(toggle);
@@ -335,10 +335,22 @@ public class LyricSearchActivity extends AppCompatActivity implements Navigation
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
 
-        int selection = 0;
         switch (item.getItemId()) {
             case R.id.lyric_navigation_help_item:
-                Toast.makeText(this, R.string.lyric_navagation_help_search, Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this);
+                helpBuilder.setTitle("Do you want to search the song?");
+                helpBuilder.setMessage("How much money do you want to donate?");
+
+                TextView textView = new TextView(this);
+                textView.setText(R.string.lyric_navagation_help_search);
+
+                // set up two buttons
+                helpBuilder.setPositiveButton("OK", null);
+
+                // create and show the dialog
+                AlertDialog helpAlertDialog = helpBuilder.create();
+                helpAlertDialog.setView(textView, 0, 0, 0, 0);
+                helpAlertDialog.show();
                 break;
             case R.id.lyric_navigation_api_item:
                 String apiLink = "https://lyricsovh.docs.apiary.io/#";
@@ -346,31 +358,31 @@ public class LyricSearchActivity extends AppCompatActivity implements Navigation
                 startActivity(launchBrower);
                 break;
             case R.id.lyric_navigation_donate_item:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Do you want to search the song?");
-                builder.setMessage("How much money do you want to donate?");
+                AlertDialog.Builder donationBuilder = new AlertDialog.Builder(this);
+                donationBuilder.setTitle("Do you want to search the song?");
+                donationBuilder.setMessage("How much money do you want to donate?");
 
                 EditText editText = new EditText(this);
                 editText.setHint(R.string.lyric_navigation_donate_number);
                 editText.setInputType(TYPE_CLASS_NUMBER);
 
                 // set up two buttons
-                builder.setPositiveButton("Thank You", null);
-                builder.setNegativeButton("Cancel", null);
+                donationBuilder.setPositiveButton("Thank You", null);
+                donationBuilder.setNegativeButton("Cancel", null);
 
                 // create and show the dialog
-                AlertDialog alertDialog = builder.create();
-                alertDialog.setView(editText, 0, 0, 0, 0);
+                AlertDialog donationAlertDialog = donationBuilder.create();
+                donationAlertDialog.setView(editText, 0, 0, 0, 0);
 
-                alertDialog.setOnShowListener(dialog -> {
+                donationAlertDialog.setOnShowListener(dialog -> {
                         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                         imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
                 });
-                alertDialog.show();
+                donationAlertDialog.show();
                 break;
         }
 
-        DrawerLayout drawerLayout = findViewById(R.id.lyric_drawer_layout);
+        DrawerLayout drawerLayout = findViewById(R.id.lyric_search_drawer_layout);
         drawerLayout.closeDrawer(GravityCompat.START);
 
         return false;
