903aa46f3e3615eee66619d6370aec126d49beb7
EricWu <ericwoodenman@gmail.com>
2020-07-29 15:03:07 -0400

    SongLyricsSearch Branch. 1. Add content to navigation bar. 2. Adjust layout.

--- app/src/main/java/com/example/cst2335finalgroupproject/SongLyricsSearch/LyricShowActivity.java
+++ app/src/main/java/com/example/cst2335finalgroupproject/SongLyricsSearch/LyricShowActivity.java
@@ -283,6 +283,7 @@ public class LyricShowActivity extends AppCompatActivity implements NavigationVi
 
                 TextView textView = new TextView(this);
                 textView.setText(R.string.lyric_navagation_help_search);
+                textView.setPadding(15,0,5,0);
 
                 // set up two buttons
                 helpBuilder.setPositiveButton("OK", null);
